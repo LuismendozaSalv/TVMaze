7803191d1a2f38e91a0e484a6d64be43e4ac2fa2
Luis Mendoza <luismendozasalv@gmail.com>
2021-11-01 00:03:03 -0400

    Show detail fragment init

--- app/src/main/java/com/mendosal/tvmaze/ui/ShowFragment.java
+++ app/src/main/java/com/mendosal/tvmaze/ui/ShowFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.mendosal.tvmaze.R;
 import com.mendosal.tvmaze.data.network.Resource;
@@ -57,18 +60,22 @@ public class ShowFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_show_list, container, false);
 
         // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            adapter = new MyShowRecyclerViewAdapter(getActivity(), showList);
-            recyclerView.setAdapter(adapter);
-            loadShows();
+        Button btn = view.findViewById(R.id.button2);
+        btn.setOnClickListener(view1 -> {
+            NavDirections action =
+                    ShowFragmentDirections.actionShowFragmentToShowDetailFragment();
+            Navigation.findNavController(view1).navigate(action);
+        });
+        Context context = view.getContext();
+        RecyclerView recyclerView = view.findViewById(R.id.list);
+        if (mColumnCount <= 1) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
         }
+        adapter = new MyShowRecyclerViewAdapter(getActivity(), showList);
+        recyclerView.setAdapter(adapter);
+        loadShows();
         return view;
     }
 
